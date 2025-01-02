dcde03106891a252c459d8b605e6b216c3e0d609
Rishit <159683639+noneclashofclans@users.noreply.github.com>
2025-01-02 14:52:26 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (25.66%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool validate (TreeNode* root, long minVal, long maxVal){
+        if (!root) return true;
+        if (root->val <= minVal || root->val >= maxVal){
+            return false;
+        }
+        return validate(root->left,minVal, root->val) and validate(root->right,root->val,maxVal);
+    }
+    bool isValidBST(TreeNode* root) {
+        
+
+        return validate(root,LONG_MIN,LONG_MAX);
+    }
+};
