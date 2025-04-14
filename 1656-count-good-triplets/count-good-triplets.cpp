7bebaec7f86348a03e9d4fb8489338c337eb7128
Rishit <159683639+noneclashofclans@users.noreply.github.com>
2025-04-14 10:56:31 +0530

    Time: 7 ms (72.26%) | Memory: 11.1 MB (11.18%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.cpp
+++ 1656-count-good-triplets/count-good-triplets.cpp
@@ -3,11 +3,26 @@ public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
         int n = arr.size();
         int count = 0;
-        for (int i = 0; i < n-2; i++){
-            for (int j = i+1; j < n-1; j++){
-                for (int k = j+1; k < n; k++){
-                    if (abs(arr[i]-arr[j]) <= a && abs(arr[j]-arr[k]) <= b && abs(arr[i]-arr[k]) <= c){
-                        count++;
+        // O(n^3)
+        // for (int i = 0; i < n-2; i++){
+        //     for (int j = i+1; j < n-1; j++){
+        //         for (int k = j+1; k < n; k++){
+        //             if (abs(arr[i]-arr[j]) <= a && abs(arr[j]-arr[k]) <= b && abs(arr[i]-arr[k]) <= c){
+        //                 count++;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // O(n^2)
+
+        for (int j = 1; j < n-1; j++){
+            for (int i = 0; i < j; i++){
+                if (abs(arr[i] - arr[j]) <= a){
+                    for (int k = j+1; k < n; k++){
+                        if (abs(arr[j] - arr[k]) <= b && abs(arr[i]-arr[k]) <= c){
+                            count++;
+                        }
                     }
                 }
             }
