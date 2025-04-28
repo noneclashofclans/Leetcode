7c230c7c02a71b63b04be0d84409d02197ebdcf8
Rishit <159683639+noneclashofclans@users.noreply.github.com>
2025-04-28 21:50:31 +0530

    Time: 5 ms (52.56%) | Memory: 45.1 MB (24.30%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,23 +1,29 @@
 class Solution {
     public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
         int ans = 0;
         int n = s.length();
-        int[] arr = new int[128];
 
-        arr['I'] = 1;
-        arr['V'] = 5;
-        arr['X'] = 10;
-        arr['L'] = 50;
-        arr['C'] = 100;
-        arr['D'] = 500;
-        arr['M'] = 1000;
+        for (int i = 0; i < n - 1; i++) {
+            int curr = map.get(s.charAt(i));
+            int next = map.get(s.charAt(i + 1));
 
-        for (int i = 0; i < n-1; i++){
-            if (arr[s.charAt(i)] < arr[s.charAt(i+1)]){
-                 ans -= arr[s.charAt(i)];
+            if (curr < next) {
+                ans -= curr;
+            } else {
+                ans += curr;
             }
-            else ans += arr[s.charAt(i)];
         }
-        return ans + arr[s.charAt(n - 1)];
+
+        ans += map.get(s.charAt(n - 1));
+        return ans;
     }
 }
