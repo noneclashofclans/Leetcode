f6a25ab0248dfeab64de8327f84d704ab92818ee
Rishit <159683639+noneclashofclans@users.noreply.github.com>
2025-10-18 14:32:48 +0530

    Time: 4 ms (7.49%) | Memory: 12.8 MB (6.97%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,18 +1,56 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
-        if (n == 0) return "";
-
-        string prefix = strs[0];
-
-        for (int i = 0; i < n; i++){
-            while (strs[i].find(prefix) != 0){
-               prefix = prefix.substr(0,prefix.size()-1); 
-
-               if (prefix.empty()) return "";
-            }
+        
+        stack<string> st;
+        for (string s : strs){
+            st.push(s);
         }
-        return prefix;
+
+        // string top = "";
+        // bool flag = true;
+
+        // while (!st.empty()){
+        //     top = st.top();
+        //     st.pop();
+            
+        //     string next = st.top();
+        //     st.pop();
+
+        //     int n = top.size();
+        //     int m = next.size();
+
+        //     int i = 0, j = 0;
+
+        //     while(i < n && j < m){
+        //         if (top[i] != next[j]){
+        //             flag = false;
+        //             break;
+        //         }
+        //         else {
+        //             ans += top[i];
+        //             i++; j++;
+        //         }
+        //     }
+
+        //     if (flag == false) break;
+        // }
+
+        string ans = st.top();
+        st.top();
+
+        while (!st.empty()){
+            string curr = st.top();
+            st.pop();
+
+            int len = min(ans.size(), curr.size());
+            int i = 0;
+            while (i < len && ans[i] == curr[i]) i++;
+
+            ans = ans.substr(0, i);
+            if (ans.empty()) return "";
+        }
+
+        return ans;
     }
 };
