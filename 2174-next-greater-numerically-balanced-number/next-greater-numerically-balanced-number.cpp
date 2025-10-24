b5c34106a3cc258a30085ab91370a3b4f6325480
Rishit <159683639+noneclashofclans@users.noreply.github.com>
2025-10-24 10:12:40 +0530

    Time: 2977 ms (5.03%) | Memory: 452.7 MB (5.03%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    const int range = 1e8;
+
+    bool satisfies(int n){
+        map<int, int> freq;
+        while (n > 0){
+            int digit = n % 10;
+            freq[digit]++;
+            n /= 10;
+        }
+
+        for (auto it = freq.begin(); it != freq.end(); ++it) {
+            int digit = it->first;
+            int count = it->second;
+            if (digit != count) return false;
+        }
+        return true;
+    }
+    int nextBeautifulNumber(int n) {
+        /*
+            the number is numerically balanced if for every digit 'd', its 
+            occurence is exactly 'd'
+
+            the starting point would be 'n'
+        */
+
+        for (int i = n + 1; i <= range; i++){
+            if (satisfies(i)) return i;
+        }
+        return -1;
+    }
+};
