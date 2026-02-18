0e7e8f6af9647559dadb1f1120804c5918446ad3
Rishit <159683639+noneclashofclans@users.noreply.github.com>
2026-02-18 14:20:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (62.63%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
-    string binary(int n){
-        string res = "";
-        while(n){
-            int remainder = n%2;
-            res += remainder;
-            n /= 2;
-        }
-        reverse(res.begin(), res.end());
-
-        return res;
-    }
+    
     bool hasAlternatingBits(int n) {
-        string binary_form = binary(n);
-        for (int i = 0; i < binary_form.size()-1; i++){
-            if(binary_form[i] == binary_form[i+1]) return false;
-        }
-        return true;
+        unsigned int x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
     }
 };
