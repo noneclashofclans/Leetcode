2e325a437b4529eb1e8c98b28a6c428d7565609f
Rishit <159683639+noneclashofclans@users.noreply.github.com>
2026-02-21 19:04:57 +0530

    Time: 3 ms (72.17%) | Memory: 7.9 MB (88.15%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool is_prime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+
+    int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            int bits = __builtin_popcount(i); 
+            if (is_prime(bits)) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
