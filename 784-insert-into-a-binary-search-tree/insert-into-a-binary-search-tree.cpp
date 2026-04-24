84d3e4c3861afa358c4ea52cb2044cc64a2f07c3
Rishit <159683639+noneclashofclans@users.noreply.github.com>
2026-04-24 13:09:56 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (92.12%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -1,24 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        TreeNode* node = new TreeNode(val);
-        if (!root) return node;
-        
-        TreeNode* current = root;
+        TreeNode* newNode = new TreeNode(val);
+        if (!root) return newNode;
 
-        while (current != NULL){
-            if (current->val > val && current->left){
-                current = current->left;
+        TreeNode* currentNode = root;
+
+        while (currentNode){
+            if (currentNode->val>val && currentNode->left){
+                currentNode = currentNode->left;
             }
-            else if (current->val < val && current->right){
-                current = current -> right;
+            else if (currentNode->val<val && currentNode->right){
+                currentNode = currentNode->right;
             }
-            else if (current->val >  val){
-                current->left = node;
+            else if (currentNode->val > val){
+                currentNode->left = newNode;
                 return root;
             }
-            else if (current->val < val){
-                current->right = node;
+            else if (currentNode->val < val){
+                currentNode->right=newNode;
                 return root;
             }
         }
